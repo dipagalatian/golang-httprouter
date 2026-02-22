33f642ca6d7c525b4806599c6aed6e9077a214ec
dipagalatian <dipagalatian3003@gmail.com>
2026-02-22 21:54:14 +0700

    FEAT: Implement router unit test with testify assert

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	// Create a new router
 	router := httprouter.New()
+
+	// Register routes and httprouter.handle
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Hello HTTPRouter GET")
+	})
 
 	server := http.Server{
 		Addr: ":8080",
---
+++ router_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRouter(t *testing.T) {
+
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, params httprouter.Params){
+		fmt.Fprint(w, "Hello from root")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello from root", string(body))
+	
+}
